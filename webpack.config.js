da1241e380a658a272f16e982d91c6abd45aa4ac
YM <yaroslav.matveev@nordigy.ru>
2016-11-25 20:25:37 +0300

    webpack config - ProvidePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+// jshint esversion: 6
+
+const webpack = require('webpack');
+const providePlugin = new webpack.ProvidePlugin({
+    'React': 'react',
+    'ReactDOM': 'react-dom',
+    'R': 'ramda'
+});
+
+const plugins = [
+    providePlugin
+];
+
 module.exports = {
     context: __dirname,
     entry: '',
+    plugins,
     output: {
         path: __dirname,
         filename: ''
